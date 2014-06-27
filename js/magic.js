af5e3a62dceeda10d662a645b6f9b4380a724912
Max Arvidsson <max.arvidsson@gmail.com>
2014-06-27 13:26:05 +0200

    Updated many spells chance that was wrong, added high elf fame protection to magic, some refactoring.

--- js/magic.js
+++ js/magic.js
@@ -1,43 +1,14 @@
-var INTEL = 1;
-var DARK = 2;
-var spells = [
-    ["Inner Sight",      220, INTEL],
-    ["Cleanse",          200, DARK],
-    ["Vision",           200, INTEL],
-    ["Poison",           180, DARK],
-    ["Enforced Honesty", 150, DARK],
-    ["Fear",             150, DARK],
-    ["Juranimosity",     150, DARK],
-    ["Lightning Bolt",   150, DARK],
-    ["Fireball",         120, DARK],
-    ["Wrath of XENE",    120, DARK],
-    ["Fly over",         100, INTEL],
-    ["Cyclops",           90, DARK],
-    ["Earthquake",        90, DARK],
-    ["Meteor Storm",      90, DARK],
-    ["Mystical Rust",     90, DARK],
-    ["Rupture",           90, DARK],
-    ["Enchantress Salem", 80, DARK],
-    ["Winds of Distress", 80, DARK],
-    ["DragonMage",        70, DARK],
-    ["Enforced Honesty",  60, DARK],
-    ["Magical Void",      60, DARK]
-];
+var tr_d = document.getElementById('target_fame_display').style.display;
+document.getElementById('target_fame_display').style.display = "none";
 
-(function(e, s){
-    var l = document.getElementById(e);
-    for (var i = 0; i < s.length; i++) {
-        l.options.add(new Option(s[i][0], i, i == 18, i == 18));
-    }
-})('spell', spells);
+function main() {
 
-function main()
-{
     var ml = Math.min(100, Math.max(1, document.getElementById('ml').value));
     var target = Math.min(100, Math.max(1, document.getElementById('target').value));
-    var spell = document.getElementById('spell').value;
-    var race = document.getElementById('race').value;
-    var target_race = document.getElementById('target_race').value;
+    var spell = getSpell('spell');
+    var race = getRace('race');
+    var target_race = getTargetRace('target_race');
+    var target_fame = Math.min(50000, Math.max(0, document.getElementById('target_fame').value));
     var churches = Math.min(25, Math.max(0, document.getElementById('churches').value));
     var sod = document.getElementById('sod').checked;
     var sci = Math.min(25, Math.max(0, document.getElementById('sci').value));
@@ -46,28 +17,25 @@ function main()
         document.getElementById('casts').value = casts;
     }
 
-    var race_fail = 0;
-    var race_cast_fail = 0;
-    var church_protection = 0.03;
-
-    switch (race) {
-        case 'nazgul':
-            race_cast_fail = 0.25;
-            break;
-    }
+    var fame_fail = 0;
     
-    switch (target_race) {
-        case 'nazgul':
-        case 'morihai':
-            race_fail = 0.25;
-            break;
+    var f = document.getElementById('target_fame_display');
+    if (target_race.ability.fame1) {
+        f.style.display = tr_d;
+        fame_fail = target_fame * 0.00001;
+    } else {
+        f.style.display = "none";
     }
+
+    var race_fail = target_race.protection;
+    var race_cast_fail = race.fumble;
+    var church_protection = 0.03;
 
     var sci_fail = sci/100;
     var church_fail = churches*church_protection;
-    var base = orkfiaSpellChance(ml, target, spells[spell][1]);
+    var base = orkfiaSpellChance(ml, target, spell.chance);
     var size_fail = 0;
-    var chance = base*(1-church_fail)*(1-sod*0.25)*(1-race_fail)*(1-race_cast_fail)*(1-sci_fail);
+    var chance = base*(1-church_fail)*(1-sod*0.25)*(1-race_fail)*(1-race_cast_fail)*(1-sci_fail)*(1-fame_fail);
 
     var canvas = document.getElementById("chart");
     var dataset = filterData(createData(casts, chance), 1);
@@ -80,15 +48,49 @@ function main()
 
 }
 
-function orkfiaSpellChance(ml, tml, spell)
-{
-    return Math.min(spell*(ml+(ml>tml)*(ml-tml)+10)/(tml+10), 290)/300;
+function orkfiaSpellChance(ml, tml, c) {
+    return Math.min(c*(ml+(ml>tml)*(ml-tml)+10)/(tml+10), 290)/300;
 }
 
-function colorResult(d)
-{
+function colorResult(d) {
     var red = 255 - Math.round(d*255);
     var green = Math.min(255, Math.round(1.5 * d * 255));
     var rgb = 'rgb(' + red + ',' + green + ',0)';
     document.getElementById('chance').style.color = rgb;
+}
+
+function selop(e) {
+    var s = document.getElementById(e);
+    return s.options[s.selectedIndex];
+}
+
+function getSpell(e) {
+    op = selop(e);
+    return {
+        chance: op.getAttribute('data-chance'),
+        type: op.getAttribute('data-type')
+    };
+}
+
+function getRace(e) {
+    op = selop(e);
+    return {
+        fumble: op.getAttribute('data-fumble') / 100
+    };
+}
+
+function getTargetRace(e) {
+    op = selop(e);
+    abilities = {fame1: false}
+    if (op.hasAttribute('data-ability')) {
+        switch (op.getAttribute('data-ability')) {
+        case 'fame1':
+            abilities.fame1 = true;
+            break;
+        }
+    }
+    return {
+        protection: op.getAttribute('data-protection') / 100,
+        ability: abilities
+    };
 }
